8f06a19f25aed5c6500d9130b4cf8319f9657f73
Diego <diegojairhl@gmail.com>
2022-03-14 01:03:15 -0500

    header implemented in index.html

--- js/header.js
+++ js/header.js
@@ -115,15 +115,15 @@ header.addEventListener("mouseleave", () => {
 });
 
 const discoverSubMenuData = [
-  { id: 1, title: "books", image: "../images/header/book.png" },
-  { id: 2, title: "films", image: "../images/header/films.webp" },
-  { id: 3, title: "portkey games", image: "../images/header/portkey-games.png" },
-  { id: 4, title: "on stage", image: "../images/header/on-stage.png" },
-  { id: 5, title: "experiences", image: "../images/header/experiences.webp" },
+  { id: 1, title: "books", image: "./images/header/book.png" },
+  { id: 2, title: "films", image: "./images/header/films.webp" },
+  { id: 3, title: "portkey games", image: "./images/header/portkey-games.png" },
+  { id: 4, title: "on stage", image: "./images/header/on-stage.png" },
+  { id: 5, title: "experiences", image: "./images/header/experiences.webp" },
 ];
 const newsFeaturesSubMenuData = [
-  { id: 10, title: "news", image: "../images/header/news.webp" },
-  { id: 11, title: "features", image: "../images/header/features.png" },
+  { id: 10, title: "news", image: "./images/header/news.webp" },
+  { id: 11, title: "features", image: "./images/header/features.png" },
 ];
 
 //Render html in submenu on desktop version
@@ -139,7 +139,7 @@ const renderHTMLSubmenu = (subMenu) => {
           <div class="card__inner-wrapper">
             <p class="card__title">${title}</p>
             <figure class="card__decoration-icon-container">
-              <img class="card__decoration-icon" src="../images/header/scarcity-icon.svg" alt="" />
+              <img class="card__decoration-icon" src="./images/header/scarcity-icon.svg" alt="" />
             </figure>
           </div>      
           </div>      
