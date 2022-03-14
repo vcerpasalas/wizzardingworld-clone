104ee3c2e7288d37541288aa9993f2c053e82f1b
Diego <diegojairhl@gmail.com>
2022-03-14 00:45:45 -0500

    header dinamico implementado

--- header/header.js
+++ header/header.js
@@ -14,20 +14,42 @@ toggleMenu.addEventListener("click", (e) => {
   }
 });
 
-// News and features subitem
+// News and features and discover subitem
 const newsFeaturesEl = document.getElementById("news-features-el");
 const newsFeaturesMenuBack = document.getElementById("news-features-back");
 const newsFeaturesContainer = document.querySelector(".news-features__menu-container");
 
+const newsFeaturesDesktopWrapper = document.getElementById("news-features-desktop-wrapper");
+const discoverDesktopWrapper = document.getElementById("discover-desktop-wrapper");
+
+const submenuArrowNewsFeatures = document.getElementById("submenu-arrow-icon-news-features");
+const submenuArrowDiscover = document.getElementById("submenu-arrow-icon-discover");
+
+const handleNewsFeaturesDesktopClick = () => {
+  newsFeaturesDesktopWrapper.classList.toggle("sub-menu__wrapper--opened");
+  discoverDesktopWrapper.classList.remove("sub-menu__wrapper--opened");
+
+  submenuArrowNewsFeatures.classList.toggle("header__submenu-arrow-icon--clicked");
+  submenuArrowDiscover.classList.remove("header__submenu-arrow-icon--clicked");
+
+  discoverEl.classList.remove("header__menu-item-text-arrow--active");
+  newsFeaturesEl.classList.toggle("header__menu-item-text-arrow--active");
+};
+
 newsFeaturesEl.addEventListener("click", () => {
-  toggleMenu.classList.add("header__menu-icon-link--hided");
-  newsFeaturesContainer.classList.add("news-features__menu-container--opened");
+  if (window.innerWidth < 1024) {
+    toggleMenu.classList.add("header__menu-icon-link--hided");
+    newsFeaturesContainer.classList.add("news-features__menu-container--opened");
+  }
+  handleNewsFeaturesDesktopClick();
 });
 
 newsFeaturesMenuBack.addEventListener("click", (e) => {
-  e.stopPropagation();
-  toggleMenu.classList.remove("header__menu-icon-link--hided");
-  newsFeaturesContainer.classList.remove("news-features__menu-container--opened");
+  if (window.innerWidth < 1024) {
+    e.stopPropagation();
+    toggleMenu.classList.remove("header__menu-icon-link--hided");
+    newsFeaturesContainer.classList.remove("news-features__menu-container--opened");
+  }
 });
 
 // Discover subitem
@@ -35,15 +57,31 @@ const discoverEl = document.getElementById("discover-el");
 const discoverMenuBack = document.getElementById("discover-back");
 const discoverContainer = document.querySelector(".discover__menu-container");
 
+const handleDiscoverDesktopClick = () => {
+  discoverDesktopWrapper.classList.toggle("sub-menu__wrapper--opened");
+  newsFeaturesDesktopWrapper.classList.remove("sub-menu__wrapper--opened");
+
+  submenuArrowDiscover.classList.toggle("header__submenu-arrow-icon--clicked");
+  submenuArrowNewsFeatures.classList.remove("header__submenu-arrow-icon--clicked");
+
+  newsFeaturesEl.classList.remove("header__menu-item-text-arrow--active");
+  discoverEl.classList.toggle("header__menu-item-text-arrow--active");
+};
+
 discoverEl.addEventListener("click", () => {
-  toggleMenu.classList.add("header__menu-icon-link--hided");
-  discoverContainer.classList.add("discover__menu-container--opened");
+  if (window.innerWidth < 1024) {
+    toggleMenu.classList.add("header__menu-icon-link--hided");
+    discoverContainer.classList.add("discover__menu-container--opened");
+  }
+  handleDiscoverDesktopClick();
 });
 
 discoverMenuBack.addEventListener("click", (e) => {
-  e.stopPropagation();
-  toggleMenu.classList.remove("header__menu-icon-link--hided");
-  discoverContainer.classList.remove("discover__menu-container--opened");
+  if (window.innerWidth < 1024) {
+    e.stopPropagation();
+    toggleMenu.classList.remove("header__menu-icon-link--hided");
+    discoverContainer.classList.remove("discover__menu-container--opened");
+  }
 });
 
 // Dynamic color on scroll to menu inner wrapper
@@ -60,23 +98,90 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// header on hover
+const header = document.getElementById("header");
+
+header.addEventListener("mouseenter", () => {
+  if (window.scrollY === 0 && window.innerWidth >= 1024) {
+    menuInnerWrapper.classList.add("header__menu-wrapper--scroll");
+    headerNavEl.classList.add("header__container--scroll");
+  }
+});
+header.addEventListener("mouseleave", () => {
+  if (window.scrollY === 0 && window.innerWidth >= 1024) {
+    menuInnerWrapper.classList.remove("header__menu-wrapper--scroll");
+    headerNavEl.classList.remove("header__container--scroll");
+  }
+});
+
+const discoverSubMenuData = [
+  { id: 1, title: "books", image: "../images/header/book.png" },
+  { id: 2, title: "films", image: "../images/header/films.webp" },
+  { id: 3, title: "portkey games", image: "../images/header/portkey-games.png" },
+  { id: 4, title: "on stage", image: "../images/header/on-stage.png" },
+  { id: 5, title: "experiences", image: "../images/header/experiences.webp" },
+];
+const newsFeaturesSubMenuData = [
+  { id: 10, title: "news", image: "../images/header/news.webp" },
+  { id: 11, title: "features", image: "../images/header/features.png" },
+];
+
 //Render html in submenu on desktop version
-const getHTMLSubmenu = (subMenu) => {
+const renderHTMLSubmenu = (subMenu) => {
   let html = "";
   subMenu.forEach((submenuEL) => {
+    const { id, image, title } = submenuEL;
     html += `
-      <div class="submenu-card" id="">
+      <div class="submenu-card" id="${id}">
           <figure class="card__image-container">
-            <img class="card__image" src="${submenuEL.image}" alt="" />
+            <img class="card__image" src="${image}" alt="" />
           </figure>
           <div class="card__inner-wrapper">
-            <p class="card__title">${submenuEL.title}</p>
+            <p class="card__title">${title}</p>
             <figure class="card__decoration-icon-container">
               <img class="card__decoration-icon" src="../images/header/scarcity-icon.svg" alt="" />
             </figure>
+          </div>      
           </div>      
       `;
   });
   return html;
 };
-//TODO: Renderizar el html en cada submenu, crear el nuevo submenu para version desktop y aplicarlo, modificar las condiciones para el anterior  menu en version mobile, ordenar imágenes y código
+
+const submenuContainerDiscover = document.getElementById("submenu-container-discover");
+let discoverSubmenuHtml = renderHTMLSubmenu(discoverSubMenuData);
+submenuContainerDiscover.innerHTML = discoverSubmenuHtml;
+
+const submenuContainerNewsFeatures = document.getElementById("submenu-container-news-features");
+let newsFeaturesSubmenuHtml = renderHTMLSubmenu(newsFeaturesSubMenuData);
+submenuContainerNewsFeatures.innerHTML = newsFeaturesSubmenuHtml;
+
+// Reset menu on some elements
+const resetMenuScreen = () => {
+  newsFeaturesDesktopWrapper.classList.remove("sub-menu__wrapper--opened");
+  discoverDesktopWrapper.classList.remove("sub-menu__wrapper--opened");
+  submenuArrowNewsFeatures.classList.remove("header__submenu-arrow-icon--clicked");
+  submenuArrowDiscover.classList.remove("header__submenu-arrow-icon--clicked");
+  discoverEl.classList.remove("header__menu-item-text-arrow--active");
+  newsFeaturesEl.classList.remove("header__menu-item-text-arrow--active");
+};
+
+window.addEventListener("mouseover", (e) => {
+  const subMenuWrapper = "sub-menu__wrapper sub-menu__wrapper--opened";
+  const headerOnScroll = "header__container header__container--scroll";
+  const value = e.target.classList.value;
+
+  if (subMenuWrapper === value || headerOnScroll === value || value === "") {
+    resetMenuScreen();
+  }
+});
+
+document.addEventListener("mouseleave", (event) => {
+  if (
+    event.clientY <= 0 ||
+    event.clientX <= 0 ||
+    event.clientX >= window.innerWidth ||
+    event.clientY >= window.innerHeight
+  )
+    resetMenuScreen();
+});
