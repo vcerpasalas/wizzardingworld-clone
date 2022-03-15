43f8c75e6d41535c715f579fe6187fd16ae42f8e
Valeria <vcerpasalas@gmail.com>
2022-03-14 23:34:16 -0500

    Change

--- apis/potter hechizos api/main.js
+++ apis/potter hechizos api/main.js
@@ -8,11 +8,9 @@ const tweetbtn = document.querySelector(".card__buttons-twitter");
 const getHechizo = async () => {
   const res = await fetch("https://vcerpasalas.github.io/wizzardingworld-clone/json/hechizos.json");
   const hechizos = await res.json();
-
   const num = Math.floor(Math.random() * hechizos.length);
 
   const item = hechizos[num];
-
   const quote = item.hechizo;
   const authorName = item.uso;
 
