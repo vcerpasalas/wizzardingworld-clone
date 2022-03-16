fe23bc31ace0eeaf890ab7681302415ef58f8961
Valeria <vcerpasalas@gmail.com>
2022-03-16 01:12:24 -0500

    Logic images js json

--- apis/potter libros api/scripts.js
+++ apis/potter libros api/scripts.js
@@ -9,20 +9,23 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open(
-  "GET",
-  "https://vcerpasalas.github.io/wizzardingworld-clone/json/libros.json",
-  true
-);
+request.open("GET", "https://vcerpasalas.github.io/wizzardingworld-clone/json/libros.json", true);
 request.onload = function () {
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
     data.forEach((book) => {
+      console.log(book);
+
       const card = document.createElement("div");
       card.setAttribute("class", "card");
 
       const h2 = document.createElement("h2");
       h2.textContent = book.title;
+
+      const imageBook = document.createElement("img");
+      imageBook.setAttribute("alt", book.title);
+      imageBook.setAttribute("src", book.image);
+      imageBook.classList.add("pokemon__card-image");
 
       const p = document.createElement("p");
       book.description = book.description.substring(0, 300);
@@ -30,6 +33,7 @@ request.onload = function () {
 
       container.appendChild(card);
       card.appendChild(h2);
+      card.appendChild(imageBook);
       card.appendChild(p);
     });
   } else {
