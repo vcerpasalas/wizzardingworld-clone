5be31dab06575e9cf3ecdbc93b8653faec54f9e1
Valeria <vcerpasalas@gmail.com>
2022-03-23 00:54:03 -0500

    Corrección linkeando páginas

--- js/headerSubmenuDesktop.js
+++ js/headerSubmenuDesktop.js
@@ -1,18 +1,18 @@
 export const discoverSubMenuDataOnDesktop = [
-  { id: 1, title: "books", image: "./images/header/book.png", link: "./apis/discover books api/index.html" },
-  { id: 2, title: "films", image: "./images/header/films.webp", link: "./apis/discover films api/index.html" },
+  { id: 1, title: "books", image: "./images/header/book.png", link: "./apis/discover-books-api/index.html" },
+  { id: 2, title: "films", image: "./images/header/films.webp", link: "./apis/discover-films-api/index.html" },
   {
     id: 3,
     title: "portkey games",
     image: "./images/header/portkey-games.png",
-    link: "./apis/discover games api/index.html",
+    link: "./apis/discover-games-api/index.html",
   },
-  { id: 4, title: "on stage", image: "./images/header/on-stage.png", link: "./apis/discover teatro api/index.html" },
+  { id: 4, title: "on stage", image: "./images/header/on-stage.png", link: "./apis/discover-teatro-api/index.html" },
   {
     id: 5,
     title: "experiences",
     image: "./images/header/experiences.webp",
-    link: "./apis/discover experiences api/index.html",
+    link: "./apis/discover-experiences-api/index.html",
   },
 ];
 export const newsFeaturesSubMenuDataOnDesktop = [
