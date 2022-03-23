b899125106e59d4019ce96cc1e17da87f3566d4d
Diego <diegojairhl@gmail.com>
2022-03-22 19:02:04 -0600

    dynamic structure for section latests posts and more posts section container

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 
 const pagesHero = document.getElementById("page-hero");
 const latestPosts = document.getElementById("latest-posts");
+const morePostsContainer = document.getElementById("more-posts-container");
 
 const renderHeroHtml = ({ displayTitle, shortDescription, mainImage }, heroPage) => {
   const { url } = mainImage.image.file;
@@ -17,51 +18,72 @@ const renderHeroHtml = ({ displayTitle, shortDescription, mainImage }, heroPage)
     `);
 };
 
-const renderLatestPostsHtml = () => {
-  latestPosts.innerHTML += `
+const renderLatestPostsHtml = ({ displayTitle }, latestData, latestPostsSection) => {
+  const secondaryPostsContainer = document.createElement("section");
+  secondaryPostsContainer.classList.add("secondary-posts-container");
+  console.log(displayTitle.toLowerCase());
+  const mainPageHref = `https://www.wizardingworld.com/${displayTitle.toLowerCase()}/`;
 
-    <h2 class="latest__title">Latest & greatest</h2>
+  latestData.forEach((element, i) => {
+    const { externalId, displayTitle, intro, mainImage } = element;
+    const { url } = mainImage.image.file;
+    if (i === 0) {
+      latestPostsSection.innerHTML += `      <h2 class="latest__title">Latest & greatest</h2>
     <article class="latest-post">
-      <figure class="latest-post__image-container"><img src="https://images.ctfassets.net/usf1vwtuqyxm/7c9HY7fJFyYNW8GrqC6UVv/bb777280eb100a1d723952520a0de23e/spells-hogwarts-legacy-approved-still.jpg?w=700&h=700&fit=fill&f=top&fm=webp" alt="" class="latest-post__image"></figure>
+      <figure class="latest-post__image-container"><img src="${url}" alt="" class="latest-post__image"></figure>
       <div class="latest-post__details-container"><h4 class="latest-post__subtitle">FEATURE SPOTLIGHT</h4> 
-        <h3 class="latest-post__title">Everything we’re excited about from the Hogwarts Legacy gameplay reveal</h3> 
-        <p class="latest-post__details">This week, Warner Bros. Games presented an exciting 14-minute preview of what to expect from Hogwarts Legacy’s gameplay – so we dived straight in to pick out some of our favourite moments.</p> 
+        <h3 class="latest-post__title">${displayTitle}</h3> 
+        <p class="latest-post__details">${intro}</p> 
         <p class="latest-post__author">By The Wizarding World Team</p> 
-        <button class="latest-post__button">Read Feature</button>
+        <a class="latest-post__button" href="${mainPageHref + externalId}" target="_blank" >Read Feature</a>
       </div>
-    </article>
-    <section class="secondary-posts-container">
+    </article>  
+     `;
+    } else {
+      secondaryPostsContainer.innerHTML += `
+        <a  href="${mainPageHref + externalId}" target="_blank">
       <article class="secondary-post">
-        <figure class="secondary-post__image-container"><img src="https://images.ctfassets.net/usf1vwtuqyxm/61Sv1uHFt9jfEYllf6jP8B/44e80498dea6c1eacc11f19b6923f361/hogwarts-legacy-gameplay-still-potions-web-landscape?w=315&h=315&fit=fill&fm=webp" alt="" class="secondary-post__image"></figure>
-        <h3 class="secondary-post__title">Go behind the scenes of Hogwarts Legacy with a new featurette</h3>
+   
+        <figure class="secondary-post__image-container"><img src="${url}" alt="" class="secondary-post__image"></figure>
+        <h3 class="secondary-post__title">${displayTitle}</h3>
          <p class="secondary-post__author">By The Wizarding World Team</p>
-      </article>
-      <article class="secondary-post">
-        <figure class="secondary-post__image-container"><img src="https://images.ctfassets.net/usf1vwtuqyxm/6Dzvr1iTOJVmCy3L0QZyg8/bfd92f50e68727b628bfdc5b7d09bbd4/dumbledore-crest.jpg?w=315&h=315&fit=fill&fm=webp" alt="" class="secondary-post__image"></figure>
-        <h3 class="secondary-post__title">First look at MinaLima’s Fantastic Beasts: The Secrets of Dumbledore graphic art prints</h3>
-         <p class="secondary-post__author">By The Wizarding World Team</p>
-      </article>
-    </section>
-    <div class="separator">
-      <hr class="separator-hr">
-      <figure class="separator-ornament__image-container"><img src="./images/ornament.svg" alt="" class="separator-ornament__image"></figure>
-      <hr class="separator-hr">
-    </div>
- `;
+         </article>
+         </a>
+
+`;
+    }
+    latestPostsSection.append(secondaryPostsContainer);
+  });
 };
 
-renderLatestPostsHtml();
+const renderMoreSectionHtml = (moreSectionData, pageSection) => {
+  // displayTitle
+  //externalId
+  moreSectionData.forEach((element) => {
+    const { displayTitle, externalId, mainImage } = element.body;
+    pageSection.innerHTML += `
+      
+       <article class="more-post">
+          <figure class="more-post__image-container"><img src="https://images.ctfassets.net/usf1vwtuqyxm/5Ni4xK7vdm4P3GmLIJOwzw/a64887996895e2375b1cd75474db12ad/5016-hpcc-220224.jpg?w=315&h=215&fit=fill&f=top&fm=webp" alt="" class="more-post__image"></figure >
+          <h3 class="more-post__title">Quiz Championship: Fantastic Beasts edition is coming soon!</h3>
+           <p class="more-post__author">By The Wizarding World Team</p>
+        </article>
+      `;
+  });
+};
 
-// TODO validación para la pagina correspondiente que queramos
 const getPagesData = async () => {
   try {
     const response = await axios.get("../json/news/newsData.json");
     const data = response.data;
-    const contentData = data.pageProps.content;
 
-    const referencedContentData = contentData.referencedContent;
-    console.log(referencedContentData);
+    const contentData = data.pageProps.content;
+    const latestContentData = contentData.referencedContent;
+    const moreContentData = data.pageProps.moreToExplore;
+
     renderHeroHtml(contentData, pagesHero);
+    renderLatestPostsHtml(contentData, latestContentData, latestPosts);
+    renderMoreSectionHtml(moreContentData, morePostsContainer);
   } catch (error) {
     console.log(error);
   }
